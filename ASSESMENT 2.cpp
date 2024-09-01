ae899478ec063306dd1611080982dbb1882cba3c
kunalmadhavai301 <kunal.madhavai24@sanjivani.edu.in>
2024-09-02 02:00:40 +0530

    Add files via upload
    In the given code of assesment 2 there are two data types i.e. integer and float and this code is used by the ordinary shopkeeper which used to do the calculation of amount of cell of his products . The multiplication operator is used in this code to get the answer....Integer is taken for the objects and float is taken for the object cost..

---
+++ ASSESMENT 2.cpp
@@ -0,0 +1,10 @@
+#include <iostream> u
+sing namespace std;
+int main() {
+int notebooks = 15;
+int pens = 20;
+float notebookCost = 2.25;
+float penCost = 0.75;
+float totalCost = (notebooks * notebookCost) + (pens * penCost);
+cout << "The total cost of the supplies is Rs. " << totalCost << endl;
+return 0; }
